a1ab3bf4b76ba95f70922ad33c79923b78b74706
Lorenzo Carlassara <lorenzo.carlassara@polimi.it>
2026-04-02 13:17:33 +0000

    add adbpo pgra2027 merged milano tr500 2m

--- run_adbpo_pgra2027.sh
+++ run_adbpo_pgra2027.sh
@@ -6,6 +6,7 @@ source "$HOME/miniforge3/etc/profile.d/conda.sh"
 conda activate ccpy4
 cd /home/admin_climatecharted_com/GitHub/flood_maps
 
-python -m adbpo_pgra2027_milano
+# python -m adbpo_pgra2027_milano_check
+python -m adbpo_pgra2027_milano_faster
 
 gsutil -m cp -r /home/admin_climatecharted_com/data/Altezza/adbpo_pgra2027_l_merged_milano_tr500_3035_2m.tif gs://cc-geodata-bucket/
